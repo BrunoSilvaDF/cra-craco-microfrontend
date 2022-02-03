7987d698ae000e708493e707bc98e028fc1cd333
Bruno Silva <brunoosilva@outlook.com>
2022-02-03 09:56:17 -0300

    config build app1

--- app1/craco.config.js
+++ app1/craco.config.js
@@ -1,39 +1,54 @@
-// const cracoModuleFederation = require('craco-module-federation')
+const { whenDev } = require('@craco/craco')
 const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin')
-const deps = require('./package.json').dependencies
+const pkgJson = require('./package.json')
+const appName = pkgJson.name
+const deps = pkgJson.dependencies
+// run
+const port = 8081
 
-module.exports = {
-  devServer: {
-    port: 8081,
-    historyApiFallback: {
-      index: '/index.html',
-    },
+const devServerConfig = {
+  port,
+  historyApiFallback: {
+    index: '/index.html',
   },
-  webpack: {
-    configure: {
-      output: {
-        publicPath: 'http://localhost:8081/',
+}
+
+const outputDevConfig = {
+  publicPath: `http://localhost:${port}/`,
+}
+
+const outputProdConfig = {
+  filename: '[name].[contenthash].js',
+  publicPath: `/${appName}/latest/`,
+}
+
+module.exports = function ({ env }) {
+  return {
+    devServer: whenDev(() => devServerConfig, []),
+    webpack: {
+      configure: {
+        output: env === 'development' ? outputDevConfig : outputProdConfig,
+        plugins: [
+          new ModuleFederation({
+            name: appName,
+            filename: 'remoteEntry.js',
+            exposes: {
+              './App1': './src/bootstrap',
+            },
+            shared: {
+              ...deps,
+              react: {
+                singleton: true,
+                requiredVersion: deps['react'],
+              },
+              'react-dom': {
+                singleton: true,
+                requiredVersion: deps['react-dom'],
+              },
+            },
+          }),
+        ],
       },
-      plugins: [
-        new ModuleFederation({
-          name: 'app1',
-          filename: 'remoteEntry.js',
-          exposes: {
-            './App1': './src/bootstrap',
-          },
-          shared: {
-            ...deps,
-            react: {
-              singleton: true,
-              requiredVersion: deps['react'],
-            },
-            'react-dom': {
-              singleton: true,
-              requiredVersion: deps['react-dom'],
-            },
-          },
-        }),
-      ],
     },
-  },
+  }
 }
